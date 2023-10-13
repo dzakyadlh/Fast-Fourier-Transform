ce04944f95584b780b2bbefbd8ba08708a5b89ce
dzakyadlh <106021129+dzakyadlh@users.noreply.github.com>
2023-10-13 14:38:12 +0700

    update, finishing mfcc

--- test.py
+++ test.py
@@ -1,62 +1,65 @@
-import numpy as np
+import librosa
+import librosa.display
 import matplotlib.pyplot as plt
-from python_speech_features import mfcc
-from python_speech_features import delta
-from python_speech_features import logfbank
+import math
 
-A = 10  # Set the limit for the x-axis
-N = 1000  # Number of data points
-x = np.linspace(-3*A, 3*A, N)  # Generate x-values within the range
 
-step_signal_1d = np.where(np.abs(x) <= 1, 1, 0)
+def calculate_mfcc(y, sr, n_mfcc=13):
+    frame_size = int(sr * 0.025)  # 25ms frame size
+    frame_stride = int(sr * 0.01)  # 10ms frame stride
+    num_frames = 1 + int((len(y) - frame_size) / frame_stride)
 
-fft_result_1d = np.fft.fftshift(np.fft.fft(step_signal_1d))
-freq_1d = np.fft.fftshift(np.fft.fftfreq(N, x[1] - x[0]))
+    # Pre-emphasis
+    pre_emphasis = 0.97
+    emphasized_signal = [y[0]] + [y[i] - pre_emphasis * y[i - 1]
+                                  for i in range(1, len(y))]
 
-step_signal_2d = np.outer(step_signal_1d, step_signal_1d)
+    # Hamming window
+    hamming_window = [
+        0.54 - 0.46 * math.cos(2 * math.pi * i / (frame_size - 1)) for i in range(frame_size)]
 
-fft_result_2d = np.fft.fftshift(np.fft.fft2(step_signal_2d))
-freq_2d = np.fft.fftshift(np.fft.fftfreq(N, x[1] - x[0]))
+    # MFCCs
+    mfccs = []
+    for frame in range(num_frames):
+        start = frame * frame_stride
+        end = start + frame_size
 
-def calculate_mfcc(signal, samplerate):
-    nfft = 512
-    mfcc_result = mfcc(signal, samplerate=samplerate, numcep=13, nfilt=26, winfunc=np.hamming, nfft=nfft)
-    return mfcc_result
+        frame_signal = emphasized_signal[start:end]
 
-samplerate = N
-mfccs = calculate_mfcc(step_signal_1d, samplerate)
+        if len(frame_signal) < frame_size:
+            frame_signal += [0] * (frame_size - len(frame_signal))
 
-plt.figure(figsize=(18, 9))
+        frame_signal = [s * w for s, w in zip(frame_signal, hamming_window)]
 
-plt.subplot(2, 2, 1)
-plt.step(x, step_signal_1d)
-plt.title('1D Step Signal with a Peak of 1')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
+        # FFT
+        spectrum = [abs(sum(frame_signal[i] * math.cos(2 * math.pi * j * i / frame_size)
+                        for i in range(frame_size))) for j in range(frame_size)]
 
-plt.subplot(2, 2, 2)
-plt.plot(freq_1d, np.abs(fft_result_1d))
-plt.title('1D FFT of the 1D Step Signal')
-plt.xlabel('Frequency')
-plt.ylabel('Amplitude')
-plt.grid()
+        # Mel filterbank
+        mel_filterbank = [list(map(lambda hz: max(0, 1 - abs(hz - (mel * (sr / 2)) / (
+            sr / frame_size))), range(frame_size))) for mel in range(n_mfcc)]
 
-plt.subplot(2, 2, 3)
-plt.imshow(np.abs(fft_result_2d), extent=(freq_2d.min(), freq_2d.max(), freq_2d.min(), freq_2d.max()))
-plt.title('2D FFT of the 2D Step Signal')
-plt.xlabel('Frequency (X)')
-plt.ylabel('Frequency (Y)')
-plt.colorbar()
-plt.grid()
+        # Log mel spectrum with epsilon to avoid math domain error
+        epsilon = 1e-10
+        mel_spectrum = [math.log(max(epsilon, sum(spectrum[i] * mel_filterbank[mel][i]
+                                 for i in range(frame_size)))) for mel in range(n_mfcc)]
 
-plt.subplot(2, 2, 4)
+        mfccs.append(mel_spectrum)
+
+    return mfccs
+
+
+# Load audio file using librosa
+# You can replace this with your audio file
+audio_file = librosa.example('trumpet')
+y, sr = librosa.load(audio_file)
+
+# Compute MFCCs
+mfccs = calculate_mfcc(y, sr)
+
+# Display the MFCCs
+plt.figure(figsize=(10, 4))
 plt.imshow(mfccs, cmap='viridis', origin='lower', aspect='auto')
-plt.title('MFCC of the 1D Step Signal')
-plt.xlabel('MFCC Coefficient')
-plt.ylabel('Frame Index')
 plt.colorbar()
-plt.grid()
-
-plt.tight_layout()
+plt.title('MFCCs')
 plt.show()
